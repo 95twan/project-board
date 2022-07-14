c6b67fef42c53be6aefa4342481c7a595da83001
taewan <95twan@gmail.com>
2022-07-15 02:35:23 +0900

    #10 - repository 작성

---
+++ src/main/java/com/twan/projectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,7 @@
+package com.twan.projectboard.repository;
+
+import com.twan.projectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
