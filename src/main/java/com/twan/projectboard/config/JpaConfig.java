3c9d5174667b950ae5f53910ca4cf3d49ab3ddb5
taewan <95twan@gmail.com>
2022-07-15 02:32:50 +0900

    #10 - Spring Data JPA 속성과 설정

---
+++ src/main/java/com/twan/projectboard/config/JpaConfig.java
@@ -0,0 +1,19 @@
+package com.twan.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("twan"); // TODO: 스프링 시큐리티 추가하고 변경예정
+    }
+
+}
