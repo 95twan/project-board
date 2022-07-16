118f66a7c1af35f6eabb3fd7b24d492df9f72fef
taewan <95twan@gmail.com>
2022-07-16 21:16:04 +0900

    #1 - data rest 설정

--- src/main/java/com/twan/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/twan/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.twan.projectboard.repository;
 
 import com.twan.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
