edb9890934beba7f0950ea2c37e76be02ce595bc
taewan <95twan@gmail.com>
2022-07-19 00:51:02 +0900

    #21 - 루트 경로를 게시판 페이지로 forward 하도록 컨트롤러 구현

---
+++ src/test/java/com/twan/projectboard/controller/MainControllerTest.java
@@ -0,0 +1,34 @@
+package com.twan.projectboard.controller;
+
+import com.twan.projectboard.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("메인 ViewController")
+@Import(SecurityConfig.class)
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+
+    private final MockMvc mvc;
+
+    public MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("root 페이지")
+    @Test
+    void test1() throws Exception {
+        // given
+
+        // when && then
+        mvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection());
+    }
+}
