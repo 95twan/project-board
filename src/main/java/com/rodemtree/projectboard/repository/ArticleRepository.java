6dd992efc8813f817d3199f1d9cf07a0a1a89da0
김태완 <95twan@naver.com>
2025-07-02 16:36:14 +0900

    #1 - data rest 설정

--- src/main/java/com/rodemtree/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/rodemtree/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.rodemtree.projectboard.repository;
 
 import com.rodemtree.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
