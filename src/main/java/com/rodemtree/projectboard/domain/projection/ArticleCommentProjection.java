5a2e41664e49938b09448c7e8e4996fbc8c6ea6b
김태완 <95twan@naver.com>
2025-07-22 20:04:51 +0900

    #93 - UserAccount api 노출 추가 + data rest projection 을 이용한 데이터 노출 필드 설정

---
+++ src/main/java/com/rodemtree/projectboard/domain/projection/ArticleCommentProjection.java
@@ -0,0 +1,19 @@
+package com.rodemtree.projectboard.domain.projection;
+
+import com.rodemtree.projectboard.domain.ArticleComment;
+import com.rodemtree.projectboard.domain.UserAccount;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.time.LocalDateTime;
+
+@Projection(name = "withUserAccount", types = ArticleComment.class)
+public interface ArticleCommentProjection {
+    Long getId();
+    UserAccount getUserAccount();
+    Long getParentCommentId();
+    String getContent();
+    LocalDateTime getCreatedAt();
+    String getCreatedBy();
+    LocalDateTime getModifiedAt();
+    String getModifiedBy();
+}
